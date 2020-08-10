a6a21cf1a791a1769550b438c0be550f70e1f99c
Napassawan <lion24092544@gmail.com>
2020-08-10 17:27:15 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Source.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+double a, b, avr, i, count = 0, sum = 0, sd, x, y = 0, z, sR;
+int main()
+{
+	scanf("%lf %lf", &a, &b);
+	if (b > a)
+	{
+		for (i = a; i <= b; i++)
+		{
+			printf("%.0lf ", i);
+			x = i * i;
+			y += x;
+			sum += i;
+			count++;
+		}
+	}
+	else
+	{
+		for (i = a; i >= b; i--)
+		{
+			printf("%.0lf ", i);
+			x = i * i;
+			y += x;
+			sum += i;
+			count++;
+		}
+	}
+	printf("\n");
+	avr = sum / count;
+	z = sum * sum;
+	sd = ((count * y) - z) / (count * (count - 1));
+	sR = sqrt(sd);
+	//printf("%.0lf\n",y);
+	printf("%.1lf\n", avr);
+	printf("%.2lf\n", sR);
+}
